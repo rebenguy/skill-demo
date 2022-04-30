7aec33009ee3b5844b51f21af008394885541e91
rebenguy <103283828+rebenguy@users.noreply.github.com>
2022-04-29 22:39:49 -0700

    Revised

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,10 +1,12 @@
 public class SkillDemo {
 
     public static int expand(int current, int minimum, int required) {
+        
         if(current < minimum) {
             current = current * 2;
         }
-        else if(current < required) {
+        
+        if(current < required) {
             current = required;
         }
 
